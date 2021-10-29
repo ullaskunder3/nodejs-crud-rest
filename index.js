0279470c08bab0a422d3ad7e779127fc785e1253
ullas <ullaskunder3@gmail.com>
2021-10-29 17:53:25 +0530

    implementing the post method

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const PORT = 3000;
 
 app.use(express.json());
 app.use('/users', userRoutes)
+app.use('/', (req, res)=>{
+    res.send("Home Page...")
+})
 
 app.listen(PORT, (req, res)=>{
     console.log('Server is running... on the PORT:', PORT);
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,24 @@
 import express from "express";
 const router = express.Router();
 
+const users = [
+    {
+        fName: "Ullas",
+        lName: "Kunder",
+        skillLevel : 8.5,
+        ranking: 3
+    }
+]
+
 router.get('/', (req, res)=>{
-    res.send("user routes test...")
+    res.send(users)
 });
+router.post('/', (req, res)=>{
+    console.log('Post is been made by the client...');
+    const user = req.body;
+    users.push(user)
+
+    res.send(`Hello ${user.fName}`);
+})
 
 export default router;
