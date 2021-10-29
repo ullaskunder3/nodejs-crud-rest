962b83a2f9ed90ab0a0e52009364351545a7b1c9
ullas <ullaskunder3@gmail.com>
2021-10-29 19:15:41 +0530

    implementing update method

--- routes/users.js
+++ routes/users.js
@@ -18,17 +18,43 @@ router.post('/', (req, res)=>{
     users.push(userWithId)
     res.send(`Hello ${user.fName}`);
 })
+
 router.get('/:id', (req, res)=>{
     const { id: searchId } = req.params;
     const founduser = users.find((user)=>user.id === searchId);
+
     res.send(founduser)
 })
+
 router.delete('/:id', (req, res)=>{
     const {id: deleteId } = req.params;
     users = users.filter((user)=>{
         user.id !== deleteId
     })
+
     res.send(`User with ${deleteId} id has been deactivated `);
+})
+
+router.patch('/:id', (req, res)=>{
+    const {id: updateUser} = req.params;
+    const {fName, lName, skillLevel, ranking} = req.body;
+    const updateUserData = users.find((user)=>user.id === updateUser)
+
+    if(fName){
+        updateUserData.fName = fName;
+    }
+    if(lName){
+        updateUserData.lName = lName;
+    }
+    if(skillLevel){
+        updateUserData.skillLevel = skillLevel;
+    }
+    if(ranking){
+        updateUserData.ranking = ranking;
+    }
+
+    res.send('User data has been updated...')
+
 })
 
 export default router;
