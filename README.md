d8968089ce5e518d754d57db9ccdf70075f892c9
ullas <ullaskunder3@gmail.com>
2021-10-29 17:25:41 +0530

    testing route & Added README

---
+++ README.md
@@ -0,0 +1,17 @@
+# RESTapi
+
+## Buidling CRUD api using nodejs & express
+
+## Routes
+
+```js
+GET/users :Find all users
+
+POST/users :Create a users
+
+GET/users/:id :Find user details
+
+DELETE/users/:id :Delete a user
+
+PATCH/users/:id :Update a user
+```
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+    console.log('route test success...');
+    res.send('HOme Page...')
+})
+
 app.listen(PORT, (req, res)=>{
     console.log('Server is running... on the PORT:', PORT);
 })
